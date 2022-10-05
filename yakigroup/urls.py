7ad9784a35a67c19cf7c897fceb758812b8ad4b5
swaijames <swaijames255@gmail.com>
2022-10-05 15:03:46 +0300

    updatee admin______dash

--- yakigroup/urls.py
+++ yakigroup/urls.py
@@ -20,13 +20,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from websiteapi import views
+from websiteapi.admin import yakigroup_site
 
 urlpatterns = [
-    path('admin_dashboard/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('websiteapi.urls')),
     path('profile/', views.ProfileView.as_view()),
     # path('dashboard/', views.DashboardView.as_view),
-    # path('dashboard/', include('websiteapi.urls')),
+    path('dashboard/', yakigroup_site.urls),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
